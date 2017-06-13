b33e11db3bffde859a57837b18ab5238ba2a9f8f
jbak17 <jbak17@student.monash.edu>
2017-06-13 17:47:59 +0300

    build repository

---
+++ src/main/scala/population/Main.scala
@@ -0,0 +1,12 @@
+package population
+
+/**
+  * Created by jeva on 13/06/17.
+  */
+
+  object Main {
+
+    def main(args: Array[String]) {
+      println("Hello, world!")
+    }
+  }
---
+++ src/test/scala/population/mainTest.scala
@@ -0,0 +1,13 @@
+package population
+
+import org.scalatest.FunSuite
+import org.scalatest._
+import org.scalatest.WordSpecLike
+import org.scalatest.Matchers
+
+/**
+  * Created by jeva on 13/06/17.
+  */
+class mainTest extends FlatSpec {
+
+}
